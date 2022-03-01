4df94126c2c18086c7205d2eab8e564ed10cb61b
Diego Aldao <diego_ind@live.com.ar>
2022-03-01 12:10:36 -0300

    creado el componente nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import styled from "styled-components";
+import Navbar from "./components/Navbar";
+
+const Wrapper = styled.main`
+  background-color: var(--color-background);
+`;
 
 const App = () => {
-  return <div></div>;
+  return (
+    <Wrapper>
+      <Navbar />
+    </Wrapper>
+  );
 };
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Nav = styled.nav`
+  background: red;
+  height: 50px;
+`;
+
+const Navbar = () => {
+  return <Nav></Nav>;
+};
+
+export default Navbar;
