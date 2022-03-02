559ea8d30fb3ca6e6daaeec6f32c34ee27be3d2d
Diego Aldao <diego_ind@live.com.ar>
2022-03-02 19:05:59 -0300

    añadido custom cursor y agregada la configuracion basica

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,25 @@ import React from "react";
 import styled from "styled-components";
 import Navbar from "./components/Navbar";
 import MainHeader from "./components/MainHeader";
+import CustomCursor from "custom-cursor-react";
+import "custom-cursor-react/dist/index.css";
 
 const Wrapper = styled.main``;
 
 const App = () => {
   return (
     <Wrapper>
+      <CustomCursor
+        targets={["a, h1"]}
+        dimensions={30}
+        fill={"#c5a47e"}
+        smoothness={{
+          movement: 0.5,
+          scale: 1,
+          opacity: 0.9,
+        }}
+        targetOpacity={0.5}
+      />
       <Navbar />
       <MainHeader />
     </Wrapper>
