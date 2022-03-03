d95cf2de9b24af8464b9bcbb3494e216432c45e6
Diego Aldao <diego_ind@live.com.ar>
2022-03-03 16:35:25 -0300

    agregado comentario sobre el hook de intersection observer

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -36,113 +36,6 @@ const HeaderImg = styled.div`
     width: 70%;
   }
 `;
-const HeaderContenido = styled.div`
-  width: 100%;
-  max-width: 540px;
-  margin: 0 auto;
-  padding: 0px 15px;
-  position: relative;
-  z-index: 4;
-  @media (min-width: 768px) {
-    max-width: 720px;
-  }
-  @media (min-width: 992px) {
-    max-width: 960px;
-  }
-  @media (min-width: 1200px) {
-    max-width: 1140px;
-  }
-  @media (min-width: 1400px) {
-    max-width: 1340px;
-  }
-`;
-const MiniTexto = styled.h5`
-  position: relative;
-  font-size: 20px;
-  padding: 0px;
-  margin: 0px 0px 10px;
-  text-transform: uppercase;
-  font-weight: 300;
-  color: var(--color-principal);
-  letter-spacing: 2px;
-  transition: all 0.4s;
-  transition-delay: 0.8s;
-  opacity: 1;
-  @media (min-width: 480px) {
-    letter-spacing: 22px;
-  }
-`;
-const Titulo = styled.h2`
-  color: #fff;
-  font-size: 28px;
-  padding: 0px;
-  margin: 0px;
-  text-transform: uppercase;
-  letter-spacing: 2px;
-  font-weight: 500;
-  display: flex;
-  align-items: center;
-  transition: all 0.3s ease-in-out;
-  text-align: left;
-  position: relative;
-  @media (min-width: 480px) {
-    font-size: 40px;
-  }
-  @media (min-width: 768px) {
-    font-size: 75px;
-  }
-`;
-const Info = styled.p`
-  position: relative;
-  font-size: 14px;
-  padding: 0px;
-  margin: 10px 0px 0px;
-  font-weight: 300;
-  line-height: 2;
-  color: var(--color-subtitulos);
-  transition: all 0.4s;
-  transition-delay: 0.8s;
-`;
-const Botton = styled.a`
-  position: relative;
-  width: 170px;
-  height: 50px;
-  background: var(--color-principal);
-  color: #fff;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-transform: capitalize;
-  margin-top: 30px;
-  font-family: var(--fuente-jost);
-  letter-spacing: 0.5px;
-  transition: all 0.8s;
-  transition-delay: 0.4s;
-  &:after {
-    content: "";
-    width: 99.5%;
-    height: 130%;
-    position: absolute;
-    top: -7px;
-    left: 0;
-    border-left: 1px solid #fff;
-    border-right: 1px solid #fff;
-    opacity: 0.6;
-    transition: all 0.3s;
-  }
-  &:before {
-    content: "";
-    width: 107%;
-    height: 99.5%;
-    position: absolute;
-    left: -5px;
-    top: 0;
-    border-top: 1px solid #fff;
-    border-bottom: 1px solid #fff;
-    opacity: 0.6;
-    transition: all 0.3s;
-  }
-`;
 
 const sliderStyles = {
   width: "100%",
@@ -154,6 +47,7 @@ const sliderStyles = {
 };
 
 const MainHeader = () => {
+  //CADA CONTENEDOR NECESITA SU PROPIO REF E INVIEW
   const [ref, inView] = useInView({
     rootMargin: "0px",
   });
