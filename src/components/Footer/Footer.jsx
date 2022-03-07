be9f61380414f8702fa7937242ca3e7074a62622
Diego Aldao <diego_ind@live.com.ar>
2022-03-07 18:23:03 -0300

    añadido la estructura basica del footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import styled from "styled-components";
 
+const Wrapper = styled.section`
+  width: 100%;
+  padding: 0px;
+  color: #fff;
+`;
+const Contenedor = styled.div`
+  padding: 0px 15px;
+  max-width: 540px;
+  margin: 0 auto;
+  @media (min-width: 768px) {
+    max-width: 720px;
+  }
+  @media (min-width: 992px) {
+    max-width: 960px;
+  }
+  @media (min-width: 1200px) {
+    max-width: 1140px;
+  }
+  @media (min-width: 1400px) {
+    max-width: 1340px;
+  }
+`;
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Wrapper>
+      <Contenedor></Contenedor>
+    </Wrapper>
+  );
 };
 
 export default Footer;
