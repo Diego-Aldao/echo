8c6b4223246f1247134d4856fd13723798e16b99
Diego Aldao <diego_ind@live.com.ar>
2022-03-18 15:38:42 -0300

    corregido el layout y agregado el componente Caracteristicas

--- src/paginas/About/Info.jsx
+++ src/paginas/About/Info.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import styled from "styled-components";
 
-const Contenido = styled.div`
-  display: flex;
-  flex-direction: column;
+const Wrapper = styled.section`
+  width: 100%;
+  padding: 140px 0px;
+  color: #fff;
+  cursor: default;
+`;
+const Contenedor = styled.div`
+  padding: 0px 15px;
+  max-width: 540px;
+  margin: 0 auto;
+  @media (min-width: 768px) {
+    max-width: 720px;
+  }
   @media (min-width: 992px) {
-    flex-direction: row;
+    max-width: 960px;
+    display: flex;
+  }
+  @media (min-width: 1200px) {
+    max-width: 1140px;
+  }
+  @media (min-width: 1400px) {
+    max-width: 1340px;
   }
 `;
 
@@ -58,41 +75,43 @@ const Descripcion = styled.div`
 
 const Info = () => {
   return (
-    <Contenido>
-      <ContenedorNumeros>
-        <Numeros>
-          <h3>352</h3>
-          <h6>projectos completados</h6>
-        </Numeros>
-        <Numeros>
-          <h3>567</h3>
-          <h6>clientes satisfechos</h6>
-        </Numeros>
-        <Numeros>
-          <h3>
-            651 <span>mil</span>
-          </h3>
-          <h6>ganancia mensual</h6>
-        </Numeros>
-        <Numeros>
-          <h3>17</h3>
-          <h6>premios ganados</h6>
-        </Numeros>
-      </ContenedorNumeros>
-      <Descripcion>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus,
-          reiciendis omnis? Sit dignissimos omnis labore aliquam error molestias
-          unde eum.
-        </p>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore porro
-          doloremque et impedit ea illo, nihil quo maxime inventore ratione, sed
-          accusantium minima ab numquam perferendis omnis recusandae fuga?
-          Nostrum doloribus illo rerum eaque sequi.
-        </p>
-      </Descripcion>
-    </Contenido>
+    <Wrapper>
+      <Contenedor>
+        <ContenedorNumeros>
+          <Numeros>
+            <h3>352</h3>
+            <h6>projectos completados</h6>
+          </Numeros>
+          <Numeros>
+            <h3>567</h3>
+            <h6>clientes satisfechos</h6>
+          </Numeros>
+          <Numeros>
+            <h3>
+              651 <span>mil</span>
+            </h3>
+            <h6>ganancia mensual</h6>
+          </Numeros>
+          <Numeros>
+            <h3>17</h3>
+            <h6>premios ganados</h6>
+          </Numeros>
+        </ContenedorNumeros>
+        <Descripcion>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus,
+            reiciendis omnis? Sit dignissimos omnis labore aliquam error
+            molestias unde eum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore
+            porro doloremque et impedit ea illo, nihil quo maxime inventore
+            ratione, sed accusantium minima ab numquam perferendis omnis
+            recusandae fuga? Nostrum doloribus illo rerum eaque sequi.
+          </p>
+        </Descripcion>
+      </Contenedor>
+    </Wrapper>
   );
 };
 
