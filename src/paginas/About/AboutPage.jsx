6e1aae515e13505e0686426095b30866a7aff6f3
Diego Aldao <diego_ind@live.com.ar>
2022-03-18 16:15:56 -0300

    añadidos los componentes testimonios y equipo

--- src/paginas/About/AboutPage.jsx
+++ src/paginas/About/AboutPage.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Caracteristicas from "./Caracteristicas/Caracteristicas";
 import Info from "./Info";
+import Testimonios from "../../components/Testimonios/Testimonios";
+import Equipo from "../../components/Equipo/Equipo";
 
 const AboutPage = () => {
   return (
     <>
       <Info />
       <Caracteristicas />
+      <Testimonios />
+      <Equipo />
     </>
   );
 };
