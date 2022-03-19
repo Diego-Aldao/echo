b477a80f0d3f52d825512225e9f6bb8b7ee51bdd
Diego Aldao <diego_ind@live.com.ar>
2022-03-19 20:43:01 -0300

    añadido el componente Intro

---
+++ src/paginas/Portfolio/Intro.jsx
@@ -0,0 +1,108 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  width: 100%;
+  padding: 140px 0px;
+  color: #fff;
+  cursor: default;
+`;
+const Contenedor = styled.div`
+  padding: 0px 15px;
+  max-width: 540px;
+  margin: 0 auto;
+  @media (min-width: 768px) {
+    max-width: 720px;
+  }
+  @media (min-width: 992px) {
+    max-width: 960px;
+    display: flex;
+  }
+  @media (min-width: 1200px) {
+    max-width: 1140px;
+  }
+  @media (min-width: 1400px) {
+    max-width: 1340px;
+  }
+`;
+
+const Info = styled.div`
+  margin-bottom: 50px;
+`;
+
+const Titulo = styled.h4`
+  font-size: 20px;
+  font-weight: 500;
+  line-height: 1.4;
+  margin: 0px 0px 20px;
+  text-transform: capitalize;
+`;
+
+const Texto = styled.p`
+  color: var(--color-parrafos);
+  font-size: 15px;
+  font-weight: 400;
+  line-height: 2;
+  margin: 0;
+  text-transform: capitalize;
+`;
+
+const ContenedorDatos = styled.div``;
+
+const Dato = styled.div`
+  margin-bottom: 30px;
+  h6 {
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 1.4;
+    margin: 0px;
+    text-transform: capitalize;
+    font-family: var(--fuente-poppins);
+  }
+  p {
+    color: var(--color-parrafos);
+    font-size: 15px;
+    font-weight: 400;
+    line-height: 2;
+    margin: 0;
+    text-transform: capitalize;
+  }
+`;
+
+const Intro = () => {
+  return (
+    <Wrapper>
+      <Contenedor>
+        <Info>
+          <Titulo>introduccion</Titulo>
+          <Texto>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis
+            omnis magni consectetur veniam ullam sint ab placeat exercitationem
+            odit, aliquam beatae, voluptate sequi praesentium consequuntur et
+            maxime amet distinctio iure!
+          </Texto>
+        </Info>
+        <ContenedorDatos>
+          <Dato>
+            <h6>cliente</h6>
+            <p>envato.com</p>
+          </Dato>
+          <Dato>
+            <h6>fecha</h6>
+            <p>6 agosto 2022</p>
+          </Dato>
+          <Dato>
+            <h6>categorias</h6>
+            <p>diseño interior, muebles</p>
+          </Dato>
+          <Dato>
+            <h6>tags</h6>
+            <p>moderno, diseño, minimalista</p>
+          </Dato>
+        </ContenedorDatos>
+      </Contenedor>
+    </Wrapper>
+  );
+};
+
+export default Intro;
--- src/paginas/Portfolio/PortfolioPage.jsx
+++ src/paginas/Portfolio/PortfolioPage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import HeaderSecundario from "../../layout/HeaderSecundario";
+import Intro from "./Intro";
 
 const PortfolioPage = () => {
-  return <></>;
+  return (
+    <>
+      <HeaderSecundario nombre={"muebles de lujo"} link={"portfolio"} />
+      <Intro />
+    </>
+  );
 };
 
 export default PortfolioPage;
