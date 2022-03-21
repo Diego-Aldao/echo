98dc7f1b9d279cbb2998ae290805fdccd6a9c43a
Diego Aldao <diego_ind@live.com.ar>
2022-03-21 16:09:10 -0300

    añadido el componente ContenidoBlog

--- src/paginas/Blog/BlogPage.jsx
+++ src/paginas/Blog/BlogPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeaderSecundario from "../../layout/HeaderSecundario";
+import ContenidoBlog from "./ContenidoBlog";
 
 const BlogPage = () => {
   return (
     <>
       <HeaderSecundario nombre={"utlimos blogs"} link={"blog"} />
+      <ContenidoBlog />
     </>
   );
 };
