358bde81445ce56ef164fa5d8375cea83bd07c5c
Diego Aldao <diego_ind@live.com.ar>
2022-03-25 18:55:09 -0300

    cambiada la configuracion de vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, resolveConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolveConfig(__dirname, "index.html"),
+        nested: resolveConfig(__dirname, "/index.html"),
+      },
+    },
+  },
 });
